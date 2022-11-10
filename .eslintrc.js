0bcb57cbd69bfde0e740a6917630d30625ce82be
lucasborges24 <lucasborgesbarbosa2012@gmail.com>
2022-11-10 12:13:23 -0300

    chore: set formaters

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,28 +1,19 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true,
-        "node": true
-    },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended"
-    ],
-    "overrides": [
-    ],
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react"
-    ],
-    "rules": {
-        "keyword-spacing": [
-            "error",
-            { "before": true, "after": true } 
-          ],
-          "react/react-in-jsx-scope": "off",
-   "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }]
-    }
-}
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ["eslint:recommended", "plugin:react/recommended"],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react"],
+  rules: {
+    "keyword-spacing": ["error", { before: true, after: true }],
+    "react/react-in-jsx-scope": "off",
+    "react/jsx-filename-extension": [1, { extensions: [".js", ".jsx"] }],
+  },
+};
