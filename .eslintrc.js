10ab16328e7a0989a6019d9b5cfd81bb322b5a55
lucasborges24 <lucasborgesbarbosa2012@gmail.com>
2022-11-14 10:06:45 -0300

    chore: ignore no-unused-vars

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,5 +16,6 @@ module.exports = {
     'react/react-in-jsx-scope': 'off',
     'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
     'react/prop-types': 0,
+    'no-unused-vars': 0,
   },
 };
