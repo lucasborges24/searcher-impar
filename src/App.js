ed434f866ac874ee6e7ca193bd7090689b612fe2
lucasborges24 <lucasborgesbarbosa2012@gmail.com>
2022-11-15 17:09:35 -0300

    deploy: prepare for deploy

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { SearchProvider } from './contexts/searchContext';
-import CardCreation from './pages/CardCreation';
 import Dashboard from './pages/Dashboard';
 
 function App() {
